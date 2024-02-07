a50e327826d1a8f0ce0e850826b3987244d0bebe
Simone Dario <simone.dario10@gmail.com>
2024-02-07 12:10:06 +0100

    timer okay, stop button

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
-let time = 25
+let time_minutes = 1;   
+let time_seconds = 30; 
+let duration = time_minutes * 60 + time_seconds;
 const timer = document.querySelector('.time-counter');
 const startButton = document.querySelector('.start-button');
+const stopButton = document.querySelector('.stop-button');
 
+function paddedFormat(num) {
+    return num < 10 ? '0' + num : num;
+}
+timer.textContent = `${paddedFormat(time_minutes)}:${paddedFormat(time_seconds)}`;
+
+function startFocus(){
+    startButton.classList.add('hidden');
+    stopButton.classList.remove('hidden');
+}
+
+function stopTimer(){
+    
+}
+
+function timeOver() {
+    alert("Frate, è l'ora di riposare!");
+}
+
+var alertShown = false;
+let min = 0;
+let sec = 0;
 function decrementTime(){
-    time--;
-    timer.innerText = String(time).padStart(5,0);
-    if (time < 0){
+    duration--;
+    min = parseInt(duration / 60);
+    sec = parseInt(duration % 60);
+
+    timer.textContent = `${paddedFormat(min)}:${paddedFormat(sec)}`;
+
+    if (duration < 0 && !alertShown){
+        timeOver();
+        alertShown = true;
         location.reload();
     }
 }
 
 startButton.addEventListener('click', function(){
+    startFocus()
+});
+
+startButton.addEventListener('click', function(){
     setInterval(decrementTime, 1000)
+});
+
+stopButton.addEventListener('click', function(){
+    stopTimer()
 });
